4089de0e2ea52752f7272d7d514f441fdfbebf11
nbitzz <77242831+nbitzz@users.noreply.github.com>
2023-01-28 16:51:33 -0800

    prob should use sveltekit for this but whatever i'm lazy so

---
+++ src/svelte/elem/UploadWindow.svelte
@@ -0,0 +1,24 @@
+<script>
+
+    // stats
+
+    let ServerStats = {}
+
+    fetch("/server").then(async (data) => {
+        ServerStats = await data.json()
+    })
+    
+</script>
+
+<div id="uploadWindow">
+    <h1>monofile</h1>
+    <p style:color="#999999">
+        <span class="number">{ServerStats.version ? `v${ServerStats.version}` : "•••"}</span>&nbsp;&nbsp—&nbsp;&nbsp;Discord based file sharing
+    </p>
+
+    
+    
+    <p style:color="#999999" style:text-align="center">
+        Hosting <span class="number" style:font-weight="600">{ServerStats.files || "•••"}</span> files
+    </p>
+</div>
