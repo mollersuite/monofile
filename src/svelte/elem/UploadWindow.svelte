62d3db6bd0b62babf50bb7e8043a01c1d2a61d50
nbitzz <77242831+nbitzz@users.noreply.github.com>
2023-01-28 22:43:33 -0800

    few more changes

--- src/svelte/elem/UploadWindow.svelte
+++ src/svelte/elem/UploadWindow.svelte
@@ -37,9 +37,14 @@
         </div>
     </div>
 
-    <div style:height="5px" />
+    <div style:height="10px" />
    
     <p style:color="#999999" style:text-align="center">
         Hosting <span class="number" style:font-weight="600">{ServerStats.files || "•••"}</span> files
+        <br />
+    </p>
+
+    <p style:color="#999999" style:text-align="center" style:font-size="12px">
+        Made with ❤ by <a href="https://github.com/nbitzz" style:font-size="12px">@nbitzz</a> — <a href="https://github.com/nbitzz/monofile" style:font-size="12px">source</a>
     </p>
 </div>
