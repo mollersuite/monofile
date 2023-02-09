b20f04f3fd48663b3b89113c4ced43172987d928
stringsplit <123121861+nbzschl@users.noreply.github.com>
2023-02-09 21:32:18 +0000

    whopper whopper whopper whopper

--- src/svelte/elem/pulldowns/Accounts.svelte
+++ src/svelte/elem/pulldowns/Accounts.svelte
@@ -1,16 +1,32 @@
 <script>
     import Pulldown from "./Pulldown.svelte"
+    import { _void } from "../transition/_void"
+
+    let targetAction
 </script>
 
 <Pulldown name="accounts">
     <div class="notLoggedIn">
-        <div>
+        <div class="container_div">
             <h1>monofile <span style:color="#999999">accounts</span></h1>
             <p class="flavor">Gain control of your uploads.</p>
-            
-            <a class="signUp" href="/signup">Sign up</a>
-            <br />
-            <a class="signIn" href="/login">Log in</a>
+
+            {#if targetAction}
+
+                <div class="fields" transition:_void|local>
+                    <input placeholder="username">
+                    <input placeholder="password" type="password">
+                    <button>{targetAction=="login" ? "Log in" : "Create account"}</button>
+                </div>
+
+            {:else}
+
+                <div class="lgBtnContainer" transition:_void|local>
+                    <button on:click={() => targetAction="login"}>Log in</button>
+                    <button on:click={() => targetAction="create"}>Sign up</button>
+                </div>
+
+            {/if}
         </div>
     </div>
 </Pulldown>
