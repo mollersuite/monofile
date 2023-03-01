98aaa02a188a466847df86c783b44ade81ec4e09
stringsplit <77242831+nbitzz@users.noreply.github.com>
2023-03-01 11:16:08 -0800

    oops forgot to add

---
+++ src/svelte/elem/prompts/account.js
@@ -0,0 +1,33 @@
+import { fetchAccountData } from "../stores.mjs"
+
+export function pwdChng(optPicker) {
+    optPicker.picker("Change password",[
+        {
+            name: "New password",
+            icon: "/static/assets/icons/change_password.svg",
+            id: "password",
+            inputSettings: {
+                password: true
+            }
+        },
+        {
+            name: "OK",
+            icon: "/static/assets/icons/update.svg",
+            description: "This will log you out of all sessions",
+            id: true
+        }
+    ]).then((exp) => {
+        if (exp && exp.selected) {
+            fetch(`/auth/change_password`,{method:"POST", body:JSON.stringify({
+                password:exp.password
+            })}).then((response) => {
+                
+                if (response.status != 200) {
+                    optPicker.picker(`${response.status} ${response.statusText}`,[])
+                }
+
+                fetchAccountData()
+            })
+        }
+    })
+}
