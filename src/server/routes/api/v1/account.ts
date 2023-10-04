087454659b382ac00abead6fc917686e9f2af9cb
linkability <146661751+linkability@users.noreply.github.com>
2023-10-04 16:10:42 +0100

    Add middleware to account api-v1

--- src/server/routes/api/v1/account.ts
+++ src/server/routes/api/v1/account.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import Files from "../../../lib/files";
 
+import { getAccount } from "../../../lib/middleware";
+
 let router = Router()
+
+router.use(getAccount)
 
 module.exports = function(files: Files) {
     return router
