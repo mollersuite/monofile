f2deea4bc2c9d4db90ce55d11b0f0bb2d0506622
stringsplit <77242831+nbitzz@users.noreply.github.com>
2023-10-29 15:26:51 +0000

    ok time to work on client

--- src/server/lib/DiscordAPI/index.ts
+++ src/server/lib/DiscordAPI/index.ts
@@ -1,1 +1,31 @@
 import { REST } from "./DiscordRequests"
+import type { APIMessage } from "discord-api-types/v10"
+
+const EXPIRE_AFTER = 20 * 60 * 1000
+
+interface MessageCacheObject {
+	expire: number,
+	object: APIMessage
+}
+
+export class Client {
+	private readonly token         : string
+	private readonly rest          : REST
+	private readonly targetChannel : string
+	private messageCache           : Map<string, MessageCacheObject> = new Map()
+
+	constructor(token: string, targetChannel: string) {
+		this.token = token
+		this.rest = new REST(token)
+		this.targetChannel = targetChannel
+	}
+
+	fetchMessage(id: string, cache: boolean = true) {
+		if (cache && this.messageCache.has(id)) {
+			let cachedMessage = this.messageCache.get(id)!
+			if (cachedMessage.expire >= Date.now()) {
+				return cachedMessage.object
+			}
+		}
+	}
+}
